df2d8ae02ecc4580e930b4737898a210208556ca
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-07-11 14:25:41 +0300

    My commit message

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const config = {
   ROOT_DIR: __dirname,
-  URL_PORT: 3044,
+  URL_PORT: 3099,
   URL_PATH: 'http://localhost',
-  DB_URL: 'mongodb://127.0.0.1:27017/wablicontest-42206',
+  DB_URL: 'mongodb://127.0.0.1:27017/wablicontest-92206',
   DEBUG_MODE: true,
   BASE_VERSION: '/v1',
   CONTROLLER_DIRECTORY: path.join(__dirname, 'controllers'),
